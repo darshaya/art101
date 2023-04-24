2b18e4e36f49eef78121e39c1adacc224f182fa9
Darshaya <darshayawashington@gmail.com>
2023-04-24 14:13:20 -0700

    PseuodoCode reupload

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
 
-// Constants
+// Tic-Tac_Toe
+// An implementation of the game
+// Darshaya Washington <dwashin1@ucsc.edu>
+//Jordan Wilson <jwilso17@ucsc.edu>
+// April 2023
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+// Tic-Tac-Toe pseudocode
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+// Display an empty 3x3 grid using array of arrays
+// Assign player 1 to X and player 2 to O
+// Player 1 goes first
+// Ask the current player to choose a square on the grid  
+    //When a player clicks on a square on the grid, mark it with the current player's symbol (X or O)
+// Check if the chosen square is already occupied
+// If the square is already occupied, ask the player to choose again
+// If the current square is not occupied, mark it with the current player's symbol (X or O)
+// Check if the current player has won the game
+    //Check if there are three in a row horizontally, vertically, or diagonally
+    //If the current player has won the game, display a message and end the game
+// Check if the game is a tie
+    //If all squares are filled and no player has won, display a message and end the game
+// If the game is not over, switch to the other player and repeat steps 14-23
---
+++ lab4/js/lab2.js
@@ -0,0 +1,19 @@
+// Tic-Tac_Toe
+// An implementation of the game
+// Darshaya Washington <dwashin1@ucsc.edu>
+// April 2024
+
+//Display an empty 3x3 grid using array of arrays
+//Assign player 1 to X and player 2 to O
+//Player 1 goes first
+//Ask the current player to choose a square on the grid  
+   // When a player clicks on a square on the grid, mark it with the current player's symbol (X or O)
+//Check if the chosen square is already occupied
+//If the square is already occupied, ask the player to choose again
+//If the current square is not occupied, mark it with the current player's symbol (X or O)
+//Check if the current player has won the game
+   // Check if there are three in a row horizontally, vertically, or diagonally
+   // If the current player has won the game, display a message and end the game
+//Check if the game is a tie
+    //If all squares are filled and no player has won, display a message and end the game
+//If the game is not over, switch to the other player and repeat steps 9-18
