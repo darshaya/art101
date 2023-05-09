76f851ef88d0607b879a8f799228bf7d473180ee
Darshaya <darshayawashington@gmail.com>
2023-05-08 18:55:45 -0700

    javascript edit of lab 8 task x

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -9,18 +9,20 @@ function isEven(x){
 console.log("Is 1 even?", isEven(1));
 console.log("Is 2 even?", isEven(2));
 
-array = [100, 81, 4, 16, 42, 144, 10000]
-console.log("My array", array);
+var makeArray = [100, 81, 4, 16, 42, 144, 10000]
+console.log("My array", makeArray);
 
-var result = array.map(isEven);
+var result = makeArray.map(isEven);
 //should return [true, false, true, true, true, true, true]
 console.log("Test of evenness of array", result);
 
-var result = array.map(function(x){
+var result = makeArray.map(function(x){
     return x ** 0.5;
 })
 //should return [10,9,2,4,6.48074069840786, 12, 100]
 console.log("Squareroot of array", result);
+
+var mapResults = ["This is an array I made: " + makeArray + ".</br/>" + "Test whether or not each element is even: " + result + ".</br>" ];
 
 var outputEl = document.getElementById("output"); 
 outputEl.innerHTML = mapResults; // put your results here
