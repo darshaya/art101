6961c9a58506fe7691ba2c0dad44d29095ca5426
Darshaya <darshayawashington@gmail.com>
2023-05-21 22:54:02 -0700

    jquery fix lab 11

---
+++ lab11/js/lab3.js
@@ -0,0 +1,11 @@
+ // index.js - Lab 11: Libraries & jQuery - experiment with features of jQuery
+// Author: Darshaya Washington
+// Date: May 16, 2023
+
+//grabs all minor sections and adds a button
+$(".minor-section").append("<button>test</button>");
+
+//adds a feature to all buttons which toggles the 'special' class on the parents
+$("button").click(function (){
+  $(this).parent().toggleClass("special");
+});
