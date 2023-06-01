beae57a1235a0a5407fe995a150f300ef8239dee
Darshaya <darshayawashington@gmail.com>
2023-05-31 22:24:30 -0700

    lab 15 edit

---
+++ lab15/js/lab.js
@@ -0,0 +1,19 @@
+// index.js -Lab 15: AJAX
+// Author: Darshaya Washington
+// Date: May 31, 2023
+
+$("#activate").on("click", function() {
+    $.ajax({
+    url: "https://yesno.wtf/api",
+    data: {},
+    type: "GET",
+    success: function(data) {
+        var i = "<img src=\"" + data.image +"\"/>";
+      $("#output").html(i);
+      $("#output").append("<h1>" + data.answer + "</h1>");
+      
+    }
+  })
+ 
+})
+
